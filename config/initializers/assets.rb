6f67540b6ac70613effc9078cc43fb557be97c02
Ariel Fogel <fogeltine@gmail.com>
2023-03-17 16:58:57 +0200

    add initial config and make minor style changes to layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -10,3 +10,10 @@ Rails.application.config.assets.version = "1.0"
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
+
+require "google/cloud/storage"
+
+Google::Cloud::Storage.configure do |config|
+  config.project_id  = "gcsbrowsertest"
+  config.credentials = Rails.application.credentials.gcskey.as_json
+end
